e7ee15f6d02b09c58b7881787582c928615d850c
David Tuite <dtuite@gmail.com>
2020-01-23 21:36:26 +0000

    Scrape multiple pages

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const rp = require('request-promise');
 const cheerio = require('cheerio');
 const _ = require('lodash');
-const url = 'http://www.dublincity.ie/swiftlg/apas/run/wphappsearchres.displayResultsURL?ResultID=5335334&StartIndex=1&SortOrder=APNID:DESC&DispResultsAs=WPHAPPSEARCHRES&BackURL=%3Ca%20href=wphappcriteria.display?paSearchKey=4583607%3ESearch%20Criteria%3C/a%3E';
+const fs = require('fs');
 
-const results = [];
+const url = (startIndex) => {
+  return `http://www.dublincity.ie/swiftlg/apas/run/wphappsearchres.displayResultsURL?ResultID=5335334&StartIndex=${startIndex}&SortOrder=APNID:DESC&DispResultsAs=WPHAPPSEARCHRES&BackURL=%3Ca%20href=wphappcriteria.display?paSearchKey=4583607%3ESearch%20Criteria%3C/a%3E`;
+};
 
-const main = async () => {
-  const html = await rp(url);
+const scrapeResults = (html) => {
   const $ = cheerio.load(html);
-  // console.log(html);
 
   const rows = $('div#bodyContent table tr').map((i, tr) => {
     return [
@@ -17,17 +17,34 @@ const main = async () => {
       $($('td', tr)[2]).text(),
     ];
   }).get();
-  // console.log('rows', rows);
 
-  _.drop(_.chunk(rows, 3)).forEach((row) => {
-    results.push({
+  return _.drop(_.chunk(rows, 3)).map((row) => {
+    return {
       path: row[0],
       description: _.trim(row[1]),
       location: _.trim(row[2]),
-    });
+    };
   });
+};
 
-  console.log(JSON.stringify(results, null, 2));
+const results = [];
+
+const main = async () => {
+  const html = await rp(url(1));
+  const pageResults = scrapeResults(html);
+  results.push(pageResults);
+
+  const nextResults = await Promise.all(_.times(20, async (i) => {
+    const html = await rp(url((i * 10) + 1));
+    const pageResults = scrapeResults(html);
+    return pageResults;
+  }));
+
+  results.push(nextResults);
+  const flatResults = _.flattenDeep(results);
+  fs.writeFileSync('searchResults.json', JSON.stringify(flatResults, null, 2));
+
+  // console.log(JSON.stringify(results, null, 2));
 };
 
 main();
